5f8b58a69438f04e65e0a8c0553047782c9628b5
tnab20022002@gmail.com <tnab20022002@gmail.com>
2020-03-01 19:41:48 +0800

    新增小怪(追擊功能)

---
+++ Assets/Scripts/AnimationScripts/Monster_walk.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster_walk : StateMachineBehaviour
+{
+    [SerializeField] float moveSpeed = 1f;
+    [SerializeField] float attackRange = 1f;
+
+    Transform player;
+    Rigidbody2D rigidbody;
+    Monster monster;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        rigidbody = animator.GetComponent<Rigidbody2D>();
+        monster = animator.GetComponent<Monster>();
+        
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (Can_Attack())
+        {
+            animator.SetTrigger("Attack");
+            return;
+        }
+
+        Vector2 target = new Vector2(player.position.x, rigidbody.position.y);
+        Vector2 newPosition = Vector2.MoveTowards(rigidbody.position, target, moveSpeed * Time.fixedDeltaTime);
+        rigidbody.MovePosition(newPosition);
+        monster.LookAt_Player(player);
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
+
+    bool Can_Attack()
+    {
+        return Mathf.Abs(rigidbody.position.x - player.position.x) < attackRange;
+    }
+}
---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    bool isFacingRight = false;
+
+    public void LookAt_Player(Transform player)
+    {
+        if (transform.position.x < player.position.x && !isFacingRight)
+        {
+            Filp();
+            Debug.Log(isFacingRight);
+        }
+        else if (transform.position.x > player.position.x && isFacingRight)
+        {
+            Filp();
+            Debug.Log(isFacingRight);
+        }
+    }
+
+    void Filp()
+    {
+        isFacingRight = !isFacingRight;
+        transform.Rotate(0f, 180f, 0f);
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+}
